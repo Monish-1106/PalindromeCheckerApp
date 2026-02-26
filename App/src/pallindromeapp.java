d2b97e4d605b744342587e5e2c8afdbd5c52d6fc
Monish-1106 <monishrajrr06@gmail.com>
2026-02-26 16:44:14 +0530

    uc6

--- App/src/pallindromeapp.java
+++ App/src/pallindromeapp.java
@@ -1,21 +1,25 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class pallindromeapp {
     static void main() {
-        String input = "noon";
+        String input = "civic";
 
         System.out.println("Input : " + input);
 
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         for (char c : input.toCharArray()) {
             stack.push(c);
+            queue.add(c);
         }
 
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while ( !stack.isEmpty()) {
+            if ( !stack.pop().equals(queue.remove())) {
                 isPalindrome = false;
                 break;
             }
